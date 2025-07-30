e10aa6862428d5f9379e144f7f947f5c4731e7b3
ManuNovas <enam_rda@yahoo.com.mx>
2025-07-30 01:00:28 -0600

    Automated test

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -17,11 +18,12 @@ from .models import Question, Choice
 #	return render(request, "polls/index.html", context)
 
 class IndexView(generic.ListView):
+	model = Question
 	template_name = "polls/index.html"
 	context_object_name = "latest_question_list"
 	
 	def get_queryset(self):
-		return Question.objects.order_by("-pub_date")[:5]
+		return Question.objects.filter(pub_date__lte = timezone.now()).order_by("-pub_date")[:5]
 
 #def detail(request, question_id):
 #	#try:
@@ -35,6 +37,9 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = "polls/detail.html"
+
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte = timezone.now())
 
 #def results(request, question_id):
 #	question = get_object_or_404(Question, pk = question_id)
